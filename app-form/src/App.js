d635e16e19c524776848a8aa644fb42140b59320
Hee Weng Sheng <wengsheng@hq.cdsb>
2021-07-16 17:15:19 +0800

    Step Form added

--- app-form/src/App.js
+++ app-form/src/App.js
@@ -5,57 +5,51 @@ import * as yup from "yup";
 import InputTextField from "./components/InputField";
 import DateField from "./components/DateField";
 
-
 const schema = yup.object({
-  fullName: yup.string().required('Name is required'),
-  age: yup.number().required("Age is required").integer("Invalid age").positive("Invalid Age"),
-  email: yup.string().email('Email must be valid (e.g abc123@gmail.com)').required('Email is required'),
-  dob: yup.date().required("Date of Birth is required")
-})
+  fullName: yup.string().required("Name is required"),
+  age: yup
+    .number()
+    .required("Age is required")
+    .integer("Invalid age")
+    .positive("Invalid Age"),
+  email: yup
+    .string()
+    .email("Email must be valid (e.g abc123@gmail.com)")
+    .required("Email is required"),
+  dob: yup.date().required("Date of Birth is required"),
+});
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Formik 
-          initialValues={{ fullName: "" , dob: "", age: "", email: ""}}
-          onSubmit={values => {alert(JSON.stringify(values, null, 2))}}
-          validationSchema = {schema}
-        > 
+        <Formik
+          initialValues={{ fullName: "", dob: "", age: "", email: "" }}
+          onSubmit={(values) => {
+            alert(JSON.stringify(values, null, 2));
+          }}
+          validationSchema={schema}
+        >
+          {(formik) => (
+            <form onSubmit={formik.handleSubmit}>
+              <InputTextField name="fullName" label="Full Name" />
 
-        {(formik) => 
-          <form onSubmit={formik.handleSubmit}>
-            
-            <InputTextField  
-              name="fullName" 
-              label="Full Name" 
-            />
+              <DateField name="dob" label="Date of Birth" />
 
-            <DateField 
-              name = 'dob'
-              label = 'Date of Birth'
-            />
+              <InputTextField name="age" label="Age" />
 
-            <InputTextField  
-              name="age" 
-              label="Age" 
-            />
+              <InputTextField name="email" label="Email" />
 
-            <InputTextField  
-              name="email" 
-              label="Email" 
-            />
-
-            <Button 
-              type="submit" 
-              color="primary" 
-              variant="contained" 
-              style={{marginTop: 30}}
-            >
-              Submit
-            </Button>
-
-          </form>}
+              <Button
+                type="submit"
+                color="primary"
+                variant="contained"
+                style={{ marginTop: 30 }}
+              >
+                Submit
+              </Button>
+            </form>
+          )}
         </Formik>
       </header>
     </div>
--- app-form/src/StepForm.js
+++ app-form/src/StepForm.js
@@ -1,10 +1,54 @@
 import React, {useState} from "react";
-import { FormikConfig, FormikValues} from "formik";
+import { Formik, FormikConfig, FormikValues} from "formik";
 
 const StepForm = ({children, initialValues, onSubmit }) => {
-    const [stepNumber, setStepNumber] = useState(0)
-    const steps = React.Children.toArray(children);
-    const step = steps[stepNumber]
+    const [stepNumber, setStepNumber] = useState(0);
+    const [snapshot, setSnapshot] = useState(initialValues);
 
-    
+    const steps = React.Children.toArray(children);
+    const step = steps[stepNumber];
+    const totalSteps = step.length;
+    const isLastStep = totalSteps - 1 === stepNumber;
+
+    const nextStep = (values) => {
+        setSnapshot(values)
+        setStepNumber(stepNumber + 1);
+    }
+
+    const previousStep = (values) => {
+        setSnapshot(values)
+        setStepNumber(stepNumber - 1);
+    }
+
+    const handleSubmit = (values, actions) => {
+        {step.props.onSubmit ? 
+            await step.props.onSubmit(values) : 
+            null
+        }
+
+       if (isLastStep){
+           return onSubmit(values, actions)
+       }
+       else{
+            actions.setTouched({});
+            nextStep(values);
+       }
+    }
+
+    return (
+        <div>
+            <Formik 
+                initialValues={{
+                    snapshot
+                }} 
+                onSubmit={handleSubmit} 
+                validationSchema={step.props.validationSchema}>
+            {(formik) => (
+                <form onSubmit={formik.handleSubmit}></form>)}
+            </Formik>
+        </div>
+    )
+
 }
+
+const Step = ({stepName = '', children}) => children
